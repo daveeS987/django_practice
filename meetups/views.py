6776a49ed50e38bb9dee4c21047d9ded4ba3667a
Davee Sok <daveesok@gmail.com>
2021-08-20 22:23:01 -0500

    put key into environment variable

--- meetups/views.py
+++ meetups/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
+# def index(request):
+#     return HttpResponse("Hello World")
+
+# address will start at templates folder
 def index(request):
-    return HttpResponse("Hello World!")
+    meetups = [
+        {
+            "title": "A first meetup", 
+            'location': 'New York', 
+            'slug':'a-first-meetup'
+        }, 
+        {
+            "title": "A second meetup", 
+            'location': 'Paris', 
+            'slug': 'a-second-meetup'
+        }
+    ]
+    return render(request, "meetups/index.html", {
+        "show_meetups": True, 
+        "meetups": meetups
+    })
+
+# this is just another way to add
+def meetup_details(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is the first meetup'
+    }
+    return render(request, 'meetups/meetup-details.html', {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
